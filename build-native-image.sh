50f4eea3f2a052e180f6135a09266d27a7a76865
Iván López <lopezi@objectcomputing.com>
2019-11-05 23:20:38 +0100

    Add --static to link libc

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,2 +1,2 @@
 ./gradlew assemble
-native-image --no-server --no-fallback --class-path build/libs/service-discovery-*-all.jar
+native-image --no-server --no-fallback --static --class-path build/libs/service-discovery-*-all.jar
